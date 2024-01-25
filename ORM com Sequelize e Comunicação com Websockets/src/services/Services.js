019093c1515e3e2f5c39b6fa42a8aba21e1bc6b1
zambiazzi <lucaszambiazzi194@gmail.com>
2024-01-25 09:36:16 -0300

    construindo rotas

--- ORM com Sequelize e Comunicação com Websockets/src/services/Services.js
+++ ORM com Sequelize e Comunicação com Websockets/src/services/Services.js
@@ -9,13 +9,13 @@ class Services {
     return dataSource[this.model].findAll();
   }
 
-  // async pegaUmRegistroPorId(id) {
-  //   return dataSource[this.model].findByPk(id);
-  // }
+  async pegaUmRegistroPorId(id) {
+    return dataSource[this.model].findByPk(id);
+  }
 
-  // async criaRegistro(dadosDoRegistro) {
-  //   return dataSource[this.model].create(dadosDoRegistro);
-  // }
+  async criaRegistro(dadosDoRegistro) {
+    return dataSource[this.model].create(dadosDoRegistro);
+  }
 
   async atualizaRegistro(dadosAtualizados, id) {
     const ListaDeRegistrosAtualizados = dataSource[this.model].update(dadosAtualizados, {
@@ -25,13 +25,13 @@ class Services {
     if (ListaDeRegistrosAtualizados[0] === 0) {
       return false;
     }
-    
+
     return true;
   }
 
-  // async excluiRegistro(id) {
-  //   return dataSource[this.model].destroy({ where: { id: id } });
-  // }
+  async excluiRegistro(id) {
+    return dataSource[this.model].destroy({ where: { id: id } });
+  }
 }
 
 module.exports = Services;
