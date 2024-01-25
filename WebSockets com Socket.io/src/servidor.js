45d0ba544ecc187f699b75b2875b76cecb345fda
zambiazzi <lucaszambiazzi194@gmail.com>
2024-01-25 16:41:32 -0300

    inicializando o socket.io

---
+++ WebSockets com Socket.io/src/servidor.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import url from 'url';
+import path from 'path';
+import http from 'http';
+import { Server } from "socket.io";
+
+const app = express();
+const PORT = process.env.PORT || 3000; // se existir uma variável de ambiente ele atribui ela à porta
+
+const caminhoAtual = url.fileURLToPath(import.meta.url); // atribui o caminho atual deste arquivo
+const diretorioPublico = path.join(caminhoAtual, "../..", "public");
+app.use(express.static(diretorioPublico)); // o express utiliza o diretorioPublico de forma estática
+
+const servidorHttp = http.createServer(app);
+
+servidorHttp.listen(PORT, () => {
+  console.log(`servidor escutando na porta ${PORT}`);
+});
+
+const io = new Server(servidorHttp);
+
+io.on("connection", () => { // o on funciona como o listen
+  console.log("Um cliente se conectou!");
+})
