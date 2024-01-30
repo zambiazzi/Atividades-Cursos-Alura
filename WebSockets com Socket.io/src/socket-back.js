90fb87b1f0dcef702584b59420ed657c3087c159
zambiazzi <lucaszambiazzi194@gmail.com>
2024-01-30 07:58:03 -0300

    organizando arquivos e eventos

--- WebSockets com Socket.io/src/socket-back.js
+++ WebSockets com Socket.io/src/socket-back.js
@@ -4,6 +4,6 @@ io.on("connection", (socket) => { // o on funciona como o listen
   console.log("Um cliente se conectou! ID:", socket.id);
 
   socket.on("texto_editor", (texto) => {
-    io.emit("texto_editor_clientes", texto);
+    socket.broadcast.emit("texto_editor_clientes", texto); // envia as informações APENAS para os outros clientes
   });
 });
