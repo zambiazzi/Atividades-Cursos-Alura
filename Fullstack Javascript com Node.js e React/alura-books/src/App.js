008b28ff2268c9d3fb2ae7288af21ca80d1f2c16
zambiazzi <lucaszambiazzi194@gmail.com>
2024-02-05 12:19:42 -0300

    configurando o global style

--- Fullstack Javascript com Node.js e React/alura-books/src/App.js
+++ Fullstack Javascript com Node.js e React/alura-books/src/App.js
@@ -6,10 +6,6 @@ const AppContainer = styled.div`
     height: 100vh;
     background-image: linear-gradient(90deg, #002F52 35%, #326589);
   }
-
-  li {
-    list-style: none;
-  }
 `
 
 function App() {
