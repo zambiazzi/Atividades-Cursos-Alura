28beaaa4eaddd801bdb405c09d57309eb65b1543
zambiazzi <lucaszambiazzi194@gmail.com>
2024-02-05 19:28:26 -0300

    criando filtro de livros

--- Fullstack Javascript com Node.js e React/alura-books/src/App.js
+++ Fullstack Javascript com Node.js e React/alura-books/src/App.js
@@ -5,7 +5,7 @@ import styled from 'styled-components';
 const AppContainer = styled.div`
     width: 100vw;
     height: 100vh;
-    background-image: linear-gradient(90deg, #002F52 35%, #326589);
+    background-image: linear-gradient(90deg, #002F52 35%, #326589 165%);
   }
 `
 
