26ca851fbc14be114f008fc640f347235f25bf0a
zambiazzi <lucaszambiazzi194@gmail.com>
2024-02-06 12:02:21 -0300

    criando estado

--- Fullstack Javascript com Node.js e React/alura-books/src/components/Search/index.js
+++ Fullstack Javascript com Node.js e React/alura-books/src/components/Search/index.js
@@ -25,10 +25,25 @@ const Subtitulo = styled.h3`
   margin-bottom: 40px;
 `
 
+const Resultado = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 20px;
+  cursor: pointer;
+  p {
+      width: 200px;
+  }
+  img {
+      width: 100px;
+  }
+  &:hover {
+      border: 1px solid white;
+  }
+`
+
 function Search() {
   const [livrosPesquisados, setLivrosPesquisados] = useState([]);
-
-  console.log(livrosPesquisados);
 
   return (
     <PesquisaContainer>
@@ -42,6 +57,12 @@ function Search() {
           setLivrosPesquisados(resultadoPesquisa)
         }}
       />
+      { livrosPesquisados.map( livro => (
+        <Resultado>
+          <p>{livro.nome}</p>
+          <img src={livro.src} alt='imagem'/>
+        </Resultado>
+      ) ) }
     </PesquisaContainer>
   )
 }
